ca2b77eb5526d89fa18f2920d0e2b2ea31cfa958
ScientistJake <warner.jacob@gmail.com>
2017-08-06 15:52:52 +0200

    update to sql

--- ui.R
+++ ui.R
@@ -3,48 +3,76 @@ library(httr)
 library(XML)
 library(shiny)
 library(ggplot2)
-library(dplyr)
 library(emoGG)
+library(jsonlite)
 library(shinythemes)
 
-drivers <- list(
-  "Fernando ALONSO",
-  "Valtteri BOTTAS" ,
-  "Marcus ERICSSON" ,
-  "Antonio GIOVINAZZI" ,
-  "Romain GROSJEAN" ,
-  "Lewis HAMILTON" ,
-  "Nico HULKENBERG",
-  "Daniil KVYAT" ,
-  "Kevin MAGNUSSEN" ,
-  "Felipe MASSA" ,
-  "Esteban OCON" ,
-  "Jolyon PALMER" ,
-  "Sergio PEREZ" ,
-  "Kimi RAIKKONEN" ,
-  "Daniel RICCIARDO" ,
-  "Carlos SAINZ" ,
-  "Lance STROLL" ,
-  "Stoffel VANDOORNE" ,
-  "Max VERSTAPPEN" ,
-  "Sebastian VETTEL" ,
-  "Pascal WEHRLEIN"
-)
+#this list will be input into the selectInput()
+drivers <- fromJSON("http://ergast.com/api/f1/drivers/.json?limit=2500")
+parsedrivers= data.frame(name=character())
+alldriversresults <- lapply(1:as.numeric(drivers[[1]][6][1]), function(i){
+  firstname <- drivers[[1]][[7]][[2]][[3]][i]
+  lastname <- toupper(drivers[[1]][[7]][[2]][[4]][i])
+  parsedrivers=rbind(parsedrivers,
+                     paste0(lastname,", ",firstname)
+  )
+  names(parsedrivers) = NULL
+  parsedrivers
+})
+AllDrivers <- unlist(lapply(alldriversresults, '[[', 1))
 
-ui <- fluidPage(theme = shinytheme("superhero"),
-  tags$head(tags$style(".rightAlign{float:right;color:#999999;}")),
-  headerPanel('Formula 1 Results'),
-  mainPanel(
-    p(HTML("Created by Jake Warner </br> More info: <a href='www.jacobwarner.net'>jacobwarner.net</a></br>
-           &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-           <a href='www.2-bitbio.com'>2-bitbio.com</a>"), class = 'rightAlign'),
-    selectInput("driver", "Select a driver", drivers, selected = NULL, multiple = FALSE,
-                selectize = TRUE, width = NULL, size = NULL),
-    width = 10),
-  mainPanel(plotOutput("plot1"),
-            h3("Driver Results Table"),
-            tableOutput("table1"),
-            width = 10)
-)
+Currentdrivers <- fromJSON("http://ergast.com/api/f1/2017/drivers/.json?limit=2500")
+parseCurrdrivers= data.frame(name=character())
+Currdriversresults <- lapply(1:as.numeric(Currentdrivers[[1]][6][1]), function(i){
+  id <- Currentdrivers[[1]][[7]][[3]][[1]][i]
+  firstname <- Currentdrivers[[1]][[7]][[3]][[5]][i]
+  lastname <- toupper(Currentdrivers[[1]][[7]][[3]][[6]][i])
+  parseCurrdrivers=rbind(parseCurrdrivers,data.frame(
+                         paste0(lastname,", ",firstname),
+                         id)
+  )
+  names(parseCurrdrivers) = NULL
+  parseCurrdrivers
+})
+CurrDrivers <- unlist(lapply(Currdriversresults, '[[', 1))
+#CurrDriverIDs <- unlist(lapply(Currdriversresults, '[[', 2))
 
-#shinyApp(ui = ui, server = server)
+
+ui <- navbarPage(theme = shinytheme("superhero"),
+  title="Formula 1 Stats",
+    tabPanel("Driver Data",
+    tags$head(tags$style(".rightAlign{float:right;color:#999999;}")),
+    tags$head(tags$script(src="maps.js")),
+    #headerPanel('Formula 1 Results'),
+    mainPanel(
+      p(HTML("Created by <a href='http://www.jacobwarner.net'>Jake Warner</a>"), class = 'rightAlign'),
+     #selectInput("driver", "Select a current driver", CurrDrivers, selected = NULL, multiple = FALSE,
+      #            selectize = TRUE, width = NULL, size = NULL),
+      checkboxInput("showonlycurrent","Limit to 2017 Drivers"),
+      uiOutput("driverSelection"),
+      sliderInput("date", label = h3("Filter by date"), 
+                  min = as.Date("1950-01-01","%Y-%m-%d"),
+                  max = as.Date(Sys.time()),
+                  value=c(as.Date("1950-01-01","%Y-%m-%d"),as.Date(Sys.time())),
+                  timeFormat="%Y", step=365),
+      #selectInput("driver", "or Select any driver", AllDrivers, selected = NULL, multiple = FALSE,
+      #            selectize = TRUE, width = NULL, size = NULL),
+     width = 10),
+   mainPanel(plotOutput("plot1"),
+             h3("Driver Results Table"),
+              tableOutput("table1"),
+              width = 10)
+    ),
+    tabPanel("Race Data",
+      selectInput("lap_season", "Select a season", c("2017"), selected = NULL, multiple = FALSE,
+                 selectize = TRUE, width = NULL, size = NULL),
+      selectInput("lap_race", "Select a race", c(1,2,3,4,5,6,7,8,9,10,11), selected = NULL, multiple = FALSE,
+                  selectize = TRUE, width = NULL, size = NULL),
+      mainPanel(h2(textOutput('track_info')),
+                h4('Lap Position'),
+                plotOutput("plot2"),
+                h4("Pace"),
+                plotOutput("plot3"),
+               width = 10)
+    )
+)
